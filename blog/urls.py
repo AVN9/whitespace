2e01ac94011288d14f8b10034a9df907918fc7d3
mohit kumar (KUMARMOH) <mohit.kumar3@amdocs.com>
2018-05-01 17:19:29 +0530

    added single article page

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,14 @@ from . import views
 app_name = "blog"
 
 urlpatterns = [
+    
+    # home - /home
     url(r'^$', views.index, name="blog_index"),
-    url(r'^articles/', views.articles, name="all_articles"),    
+    
+    # articles listing - /articles
+    url(r'^articles/', views.articles, name="all_articles"),
+
+    # specific article - /articles/100
+    url(r'^article/(?P<article_id>[0-9]+)$', views.article_page, name="article_page"),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Article, User
@@ -26,3 +26,8 @@ def articles(request):
         # in case out of pages, deliver last page
         articles = paginator.page(paginator.num_pages)
     return render(request, 'blog/blog_all_articles.html', {'articles' : articles})
+
+# for printing a single article using its id
+def article_page(request, article_id):
+    article = get_object_or_404(Article, id=article_id)
+    return render(request, 'blog/blog_article.html', {'article' : article})
