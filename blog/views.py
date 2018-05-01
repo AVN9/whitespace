562c5930320d1074f68c9ddaa89c464d2eddde61
mohit kumar (KUMARMOH) <mohit.kumar3@amdocs.com>
2018-05-01 15:51:00 +0530

    added pagination to articles

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Article, User
 
 # Create your views here.
@@ -11,5 +12,17 @@ def index(request):
 
 # for printing articles
 def articles(request):
-    article_obj = Article.objects.all()[:10]
-    return render(request, 'blog/blog_all_articles.html', { 'articles' : article_obj})
+    article_list = Article.objects.all()
+    # listing only 10 articles per page
+    paginator = Paginator(article_list, 10)
+    
+    page_req = request.GET.get('page')
+    try:
+        articles = paginator.page(page_req)
+    except PageNotAnInteger:
+        # if page is not an integer deliver first page
+        articles = paginator.page(1)
+    except EmptyPage:
+        # in case out of pages, deliver last page
+        articles = paginator.page(paginator.num_pages)
+    return render(request, 'blog/blog_all_articles.html', {'articles' : articles})
